33e4b28f28408b13d060787f9d621693bb3c96b2
Leonel Uzcategui <129588783+leonelenrique141@users.noreply.github.com>
2023-04-08 13:36:58 -0400

    Agregando ejercicios

---
+++ DÍA 3/Práctica Listas 1.py
@@ -0,0 +1,4 @@
+'''Práctica Listas 1 Crea una lista con 5 elementos, dentro de la variable mi_lista. Puedes incluir strings, booleanos, números, etc.'''
+
+lista = ['palabra', True, 2, 6.5, 'prueba']
+print(lista)
